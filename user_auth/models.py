15135ecb88997ecb38983777f6609ac92abb4bb4
nirmalks <kagosekar@gmail.com>
2017-09-24 13:49:05 +0530

    1.Added user_auth app to settings 2.Added user model 3.Applied migration

--- user_auth/models.py
+++ user_auth/models.py
@@ -4,3 +4,8 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+
+class User(models.Model):
+    username = models.CharField(max_length=50)
+    email = models.CharField(max_length=100)
+    password = models.CharField(max_length=100)
