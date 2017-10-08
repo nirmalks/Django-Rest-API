148b653ca6f234d651805ba9d747720304c927b0
nirmalks <kagosekar@gmail.com>
2017-10-08 13:33:56 +0530

    1. Refactored Api - Added Status code

--- user_auth/views.py
+++ user_auth/views.py
@@ -4,13 +4,15 @@ from __future__ import unicode_literals
 from rest_framework import serializers , generics
 from .serializers import UserSerializer
 from .models import User
-from rest_framework.views import APIView
-from django.http import HttpResponse, JsonResponse
+from rest_framework import status
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
+@api_view(['GET'])
 def view_users(request):
 
     if request.method == 'GET':
         users = User.objects.all()
         serializer = UserSerializer(users, many=True)
-        return JsonResponse(serializer.data, safe=False)
+        return Response(serializer.data , status=status.HTTP_200_OK)
 
