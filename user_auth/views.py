4be905139f7e6dee40767ea2dd2f364cd4cf27da
nirmalks <kagosekar@gmail.com>
2017-10-08 12:49:53 +0530

    1.Added REST support for user class 2. Added API to view users

--- user_auth/views.py
+++ user_auth/views.py
@@ -1,6 +1,16 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from django.shortcuts import render
+from rest_framework import serializers , generics
+from .serializers import UserSerializer
+from .models import User
+from rest_framework.views import APIView
+from django.http import HttpResponse, JsonResponse
 
-# Create your views here.
+def view_users(request):
+
+    if request.method == 'GET':
+        users = User.objects.all()
+        serializer = UserSerializer(users, many=True)
+        return JsonResponse(serializer.data, safe=False)
+
