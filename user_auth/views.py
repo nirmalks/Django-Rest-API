abaa6c1c442d55047d850c2ba4f7c7919175faa0
nirmalks <kagosekar@gmail.com>
2017-10-08 16:54:59 +0530

    Added view for user-details

--- user_auth/views.py
+++ user_auth/views.py
@@ -7,6 +7,7 @@ from .models import User
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
+from django.http import Http404
 
 class UsersList(APIView):
 
@@ -14,5 +15,39 @@ class UsersList(APIView):
         users = User.objects.all()
         serializer = UserSerializer(users, many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
+
+class UserDetails(APIView):
+    def get_user(self , pk):
+        try:
+            return User.objects.get(pk = pk)
+        except User.DoesNotExist:
+            raise Http404
+
+    def get(self , request , pk , format = None):
+        user = self.get_object(pk)
+        serializer = UserSerializer(user)
+        return Response(serializer.data)
+
+    def post(self, request , pk , format = None):
+        user = self.get_object(pk)
+        serializer = UserSerializer(user , data = request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def put(self, request , pk , format = None):
+        user = self.get_object(pk)
+        serializer = UserSerializer(user , data = request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request , pk , format = None):
+        user = self.get_object(pk)
+        user.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
 
 
